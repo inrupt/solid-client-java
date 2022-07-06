63c36153570b609e5386f6ae806b0abc63018610
Aaron Coburn <aaronc@inrupt.com>
2022-07-06 14:35:17 -0500

    Add jena body subscriber implementations

--- jena/src/main/java/com/inrupt/client/jena/JenaBodySubscribers.java
+++ jena/src/main/java/com/inrupt/client/jena/JenaBodySubscribers.java
@@ -20,40 +20,99 @@
  */
 package com.inrupt.client.jena;
 
+import java.io.InputStream;
 import java.net.http.HttpResponse;
 
 import org.apache.jena.graph.Graph;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 
+/**
+ * Classes for reading HTTP responses as Jena objects.
+ */
 public final class JenaBodySubscribers {
 
+    /**
+     * Process an HTTP response as a Jena {@link Model}.
+     *
+     * <p>This method expects the default (TURTLE) serialization of an HTTP response.
+     *
+     * @param model the model into which to populate the response
+     * @return the body subscriber
+     */
     public static HttpResponse.BodySubscriber<Model> ofModel(final Model model) {
         return ofModel(model, Lang.TURTLE);
     }
 
+    /**
+     * Process an HTTP response as a Jena {@link Model}.
+     *
+     * @param model the model into which to populate the response
+     * @param lang the RDF serialization of the HTTP response
+     * @return the body subscriber
+     */
     public static HttpResponse.BodySubscriber<Model> ofModel(final Model model, final Lang lang) {
-        // TODO implement
-        return null;
+        final var upstream = HttpResponse.BodySubscribers.ofInputStream();
+        return HttpResponse.BodySubscribers.mapping(upstream, (InputStream input) -> {
+            RDFDataMgr.read(model, input, lang);
+            return model;
+        });
     }
 
+    /**
+     * Process an HTTP response as a Jena {@link Graph}.
+     *
+     * <p>This method expects the default (TURTLE) serialization of an HTTP response.
+     *
+     * @param graph the graph into which to populate the response
+     * @return the body subscriber
+     */
     public static HttpResponse.BodySubscriber<Graph> ofGraph(final Graph graph) {
         return ofGraph(graph, Lang.TURTLE);
     }
 
+    /**
+     * Process an HTTP response as a Jena {@link Graph}.
+     *
+     * @param graph the graph into which to populate the response
+     * @param lang the RDF serialization of the HTTP response
+     * @return the body subscriber
+     */
     public static HttpResponse.BodySubscriber<Graph> ofGraph(final Graph graph, final Lang lang) {
-        // TODO implement
-        return null;
+        final var upstream = HttpResponse.BodySubscribers.ofInputStream();
+        return HttpResponse.BodySubscribers.mapping(upstream, (InputStream input) -> {
+            RDFDataMgr.read(graph, input, lang);
+            return graph;
+        });
     }
 
+    /**
+     * Process an HTTP response as a Jena {@link Dataset}.
+     *
+     * <p>This method expects the default (TRIG) serialization of an HTTP response.
+     *
+     * @param dataset the dataset into which to populate the response
+     * @return the body subscriber
+     */
     public static HttpResponse.BodySubscriber<Dataset> ofDataset(final Dataset dataset) {
-        return ofDataset(dataset, Lang.TURTLE);
+        return ofDataset(dataset, Lang.TRIG);
     }
 
+    /**
+     * Process an HTTP response as a Jena {@link Dataset}.
+     *
+     * @param dataset the dataset into which to populate the response
+     * @param lang the RDF serialization of the HTTP response
+     * @return the body subscriber
+     */
     public static HttpResponse.BodySubscriber<Dataset> ofDataset(final Dataset dataset, final Lang lang) {
-        // TODO implement
-        return null;
+        final var upstream = HttpResponse.BodySubscribers.ofInputStream();
+        return HttpResponse.BodySubscribers.mapping(upstream, (InputStream input) -> {
+            RDFDataMgr.read(dataset, input, lang);
+            return dataset;
+        });
     }
 
     private JenaBodySubscribers() {
