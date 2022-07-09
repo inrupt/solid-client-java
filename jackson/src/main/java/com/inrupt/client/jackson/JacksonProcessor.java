34339a4d9746202cadf365f40e0f424494b5dacb
Aaron Coburn <aaronc@inrupt.com>
2022-07-09 09:18:36 -0500

    Fix jackson package name

--- jackson/src/main/java/com/inrupt/client/jackson/JacksonProcessor.java
+++ jackson/src/main/java/com/inrupt/client/jackson/JacksonProcessor.java
@@ -18,7 +18,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.inrupt.client.spi;
+package com.inrupt.client.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
