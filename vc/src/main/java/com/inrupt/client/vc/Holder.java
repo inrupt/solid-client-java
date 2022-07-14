16e58a39991f61e6e0c0a94e697e5443b0c8ecaf
Aaron Coburn <aaronc@inrupt.com>
2022-07-14 11:46:42 -0500

    Add VC Holder documentation

--- vc/src/main/java/com/inrupt/client/vc/Holder.java
+++ vc/src/main/java/com/inrupt/client/vc/Holder.java
@@ -30,10 +30,12 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
 
 /**
  * A class for interacting with a VC-API Holder endpoint.
@@ -44,6 +46,8 @@ public class Holder {
 
     private static final String CONTENT_TYPE = "Content-Type";
     private static final String APPLICATION_JSON = "application/json";
+    private static final String CREDENTIALS = "credentials";
+    private static final String PRESENTATIONS = "presentations";
 
     private final URI baseUri;
     private final HttpClient httpClient;
@@ -70,35 +74,115 @@ public class Holder {
         this.processor = ServiceProvider.getJsonProcessor();
     }
 
+    /**
+     * List all verifiable credentials.
+     *
+     * @return a list of verifiable credentials
+     */
+    public List<VerifiableCredential> listCredentials() {
+        return listCredentials(Collections.emptyList());
+    }
+
+    /**
+     * List all verifiable credentials, filtered by a list of types.
+     *
+     * @param types the VC types on which to filter
+     * @return a list of verifiable credentials
+     */
     public List<VerifiableCredential> listCredentials(final List<URI> types) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getCredentialListEndpoint(types)).build();
+        try {
+            final var res = httpClient.send(req, HttpResponse.BodyHandlers.ofInputStream());
+            return processor.fromJson(res.body(), VerifiableCredentialList.class);
+        } catch (final InterruptedException | IOException ex) {
+            throw new VerifiableCredentialException("Error listing credentials.", ex);
+        }
     }
 
+    /**
+     * List all verifiable credentials.
+     *
+     * @return the next stage of completion, including the list of verifiable credentials
+     */
+    public CompletionStage<List<VerifiableCredential>> listCredentialsAsync() {
+        return listCredentialsAsync(Collections.emptyList());
+    }
+
+    /**
+     * List all verifiable credentials, filtered by a list of types.
+     *
+     * @param types the VC types on which to filter
+     * @return the next stage of completion, including the list of verifiable credentials
+     */
     public CompletionStage<List<VerifiableCredential>> listCredentialsAsync(final List<URI> types) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getCredentialListEndpoint(Objects.requireNonNull(types))).build();
+        return httpClient.sendAsync(req, HttpResponse.BodyHandlers.ofInputStream())
+            .thenApply(res -> processor.fromJson(res.body(), VerifiableCredentialList.class));
     }
 
+    /**
+     * Retrieve a verifiable credential.
+     *
+     * @param credentialId the credential identifier
+     * @return the verifiable credential
+     */
     public VerifiableCredential getCredential(final String credentialId) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getCredentialEndpoint(Objects.requireNonNull(credentialId))).build();
+        try {
+            return httpClient.send(req, VerifiableCredentialBodyHandlers.ofVerifiableCredential()).body();
+        } catch (final InterruptedException | IOException ex) {
+            throw new VerifiableCredentialException("Error retrieving credential.", ex);
+        }
     }
 
+    /**
+     * Retrieve a verifiable credential.
+     *
+     * @param credentialId the credential identifier
+     * @return the next stage of completion, including a verifiable credential
+     */
     public CompletionStage<VerifiableCredential> getCredentialAsync(final String credentialId) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getCredentialEndpoint(Objects.requireNonNull(credentialId))).build();
+        return httpClient.sendAsync(req, VerifiableCredentialBodyHandlers.ofVerifiableCredential())
+            .thenApply(HttpResponse::body);
     }
 
+    /**
+     * Delete a credential.
+     *
+     * @param credentialId the credential identifier
+     */
     public void deleteCredential(final String credentialId) {
-        // TODO - implement
+        final var req = HttpRequest.newBuilder(getCredentialEndpoint(Objects.requireNonNull(credentialId)))
+            .DELETE().build();
+
+        try {
+            httpClient.send(req, HttpResponse.BodyHandlers.discarding());
+        } catch (final InterruptedException | IOException ex) {
+            throw new VerifiableCredentialException("Error retrieving credential.", ex);
+        }
     }
 
+    /**
+     * Delete a credential.
+     *
+     * @param credentialId the credential identifier
+     * @return the next stage of compltion
+     */
     public CompletionStage<Void> deleteCredentialAsync(final String credentialId) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getCredentialEndpoint(Objects.requireNonNull(credentialId)))
+            .DELETE().build();
+
+        return httpClient.sendAsync(req, HttpResponse.BodyHandlers.discarding())
+            .thenApply(HttpResponse::body);
     }
 
+    /**
+     * Derive a credential.
+     *
+     * @param request the selective disclosure request
+     * @return the derived verifiable credential
+     */
     public VerifiableCredential derive(final DerivationRequest request) {
         final var req = HttpRequest.newBuilder(getDeriveEndpoint())
             .header(CONTENT_TYPE, APPLICATION_JSON)
@@ -112,6 +196,12 @@ public class Holder {
         }
     }
 
+    /**
+     * Derive a credential.
+     *
+     * @param request the selective disclosure request
+     * @return the next stage of completion, including the derived verifiable credential
+     */
     public CompletionStage<VerifiableCredential> deriveAsync(final DerivationRequest request) {
         final var req = HttpRequest.newBuilder(getDeriveEndpoint())
             .header(CONTENT_TYPE, APPLICATION_JSON)
@@ -122,26 +212,84 @@ public class Holder {
             .thenApply(HttpResponse::body);
     }
 
+    /**
+     * Retrieve a list of presentations.
+     *
+     * @return a list of presentations
+     */
+    public List<VerifiablePresentation> listPresentations() {
+        return listPresentations(Collections.emptyList());
+    }
+
+    /**
+     * Retrieve a list of presentations.
+     *
+     * @param types presentation type filters
+     * @return a list of presentations
+     */
     public List<VerifiablePresentation> listPresentations(final List<URI> types) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getPresentationListEndpoint(types)).build();
+        try {
+            final var res = httpClient.send(req, HttpResponse.BodyHandlers.ofInputStream());
+            return processor.fromJson(res.body(), VerifiablePresentationList.class);
+        } catch (final InterruptedException | IOException ex) {
+            throw new VerifiableCredentialException("Error listing credentials.", ex);
+        }
     }
 
+    /**
+     * Retrieve a list of presentations.
+     *
+     * @return the next stage of completion, including a list of presentations
+     */
+    public CompletionStage<List<VerifiablePresentation>> listPresentationsAsync() {
+        return listPresentationsAsync(Collections.emptyList());
+    }
+
+    /**
+     * Retrieve a list of presentations.
+     *
+     * @param types presentation type filters
+     * @return the next stage of completion, including a list of presentations
+     */
     public CompletionStage<List<VerifiablePresentation>> listPresentationsAsync(final List<URI> types) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getPresentationListEndpoint(types)).build();
+        return httpClient.sendAsync(req, HttpResponse.BodyHandlers.ofInputStream())
+            .thenApply(res -> processor.fromJson(res.body(), VerifiablePresentationList.class));
     }
 
+    /**
+     * Retrieve a verifiable presentation.
+     *
+     * @param presentationId the presentation identifier
+     * @return the verifiable presentation
+     */
     public VerifiablePresentation getPresentation(final String presentationId) {
-        // TODO - implement
-        return null;
+        final var req = HttpRequest.newBuilder(getPresentationEndpoint(Objects.requireNonNull(presentationId))).build();
+        try {
+            return httpClient.send(req, VerifiableCredentialBodyHandlers.ofVerifiablePresentation()).body();
+        } catch (final InterruptedException | IOException ex) {
+            throw new VerifiableCredentialException("Error retrieving presentation.", ex);
+        }
     }
 
-    public CompletionStage<VerifiablePresentation> getPresenationAsync(final String presentationId) {
-        // TODO - implement
-        return null;
+    /**
+     * Retrieve a verifiable presentation.
+     *
+     * @param presentationId the presentation identifier
+     * @return the next stage of completion, including the verifiable presentation
+     */
+    public CompletionStage<VerifiablePresentation> getPresentationAsync(final String presentationId) {
+        final var req = HttpRequest.newBuilder(getPresentationEndpoint(Objects.requireNonNull(presentationId))).build();
+        return httpClient.sendAsync(req, VerifiableCredentialBodyHandlers.ofVerifiablePresentation())
+            .thenApply(HttpResponse::body);
     }
 
+    /**
+     * Delete a presentation.
+     *
+     * @param presentationId the presentation identifier
+     */
     public void deletePresentation(final String presentationId) {
         final var uri = URIBuilder.newBuilder(baseUri).path("credentials")
             .path(Objects.requireNonNull(presentationId)).build();
@@ -154,6 +302,12 @@ public class Holder {
         }
     }
 
+    /**
+     * Delete a presentation.
+     *
+     * @param presentationId the presentation identifier
+     * @return the next stage of completion
+     */
     public CompletionStage<Void> deletePresentationAsync(final String presentationId) {
         final var uri = URIBuilder.newBuilder(baseUri).path("credentials")
             .path(Objects.requireNonNull(presentationId)).build();
@@ -268,11 +422,37 @@ public class Holder {
                 IOUtils.pipe(out -> processor.toJson(request, out)));
     }
 
+    private URI getPresentationListEndpoint(final List<URI> types) {
+        return URIBuilder.newBuilder(baseUri).path(PRESENTATIONS)
+            .queryParam("type", types.stream().map(URI::toString).collect(Collectors.joining(",")))
+            .build();
+    }
+
+    private URI getCredentialListEndpoint(final List<URI> types) {
+        return URIBuilder.newBuilder(baseUri).path(CREDENTIALS)
+            .queryParam("type", types.stream().map(URI::toString).collect(Collectors.joining(",")))
+            .build();
+    }
+
+    private URI getPresentationEndpoint(final String presentationId) {
+        return URIBuilder.newBuilder(baseUri).path(PRESENTATIONS).path(presentationId).build();
+    }
+
+    private URI getCredentialEndpoint(final String credentialId) {
+        return URIBuilder.newBuilder(baseUri).path(CREDENTIALS).path(credentialId).build();
+    }
+
     private URI getDeriveEndpoint() {
-        return URIBuilder.newBuilder(baseUri).path("credentials/derive").build();
+        return URIBuilder.newBuilder(baseUri).path(CREDENTIALS).path("derive").build();
     }
 
     private URI getProveEndpoint() {
-        return URIBuilder.newBuilder(baseUri).path("presentations/prove").build();
+        return URIBuilder.newBuilder(baseUri).path(PRESENTATIONS).path("prove").build();
+    }
+
+    interface VerifiablePresentationList extends List<VerifiablePresentation> {
+    }
+
+    interface VerifiableCredentialList extends List<VerifiableCredential> {
     }
 }
