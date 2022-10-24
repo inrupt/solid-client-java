10a04d21286ffdd58c7582f8d9b0e408853a1c35
Timea <4144203+timea-solid@users.noreply.github.com>
2022-10-24 14:43:59 +0200

    JCL-144: json javadoc (#106)

--- jackson/src/main/java/com/inrupt/client/jackson/package-info.java
+++ jackson/src/main/java/com/inrupt/client/jackson/package-info.java
@@ -24,5 +24,27 @@
  * <p>Many of the high-level APIs in the Inrupt client libraries make use of a
  * JSON parser. In the Java ecosystem, there are several widely used JSON
  * parsing libraries. This package adds support for the Jackson JSON libraries.
+ *
+ * <p>A user of this module should ensure that the Jackson implementation is
+ * available on the classpath by adding the following dependency:
+ *
+ * <pre>
+ *     &lt;dependency&gt;
+ *            &lt;groupId&gt;com.inrupt&lt;/groupId&gt;
+ *            &lt;artifactId&gt;inrupt-client-jackson&lt;/artifactId&gt;
+ *            &lt;version&gt;${project.version}&lt;/version&gt;
+ *     &lt;/dependency&gt;
+ * </pre>
+ *
+ * <h3>Example of using the JSON processor fromJson() method to read a {@code VerifiableCredential}:</h3>
+ *
+ * <pre>{@code
+    JsonProcessor processor = ServiceProvider.getJsonProcessor();
+    VerifiableCredential vc;
+    try (InputStream is = Test.class.getResourceAsStream("verifiableCredential.json")) {
+        vc = processor.fromJson(is, VerifiableCredential.class);
+    }
+    System.out.println("The Verifiable Credential Id is: " + vc.id);
+ * }</pre>
  */
 package com.inrupt.client.jackson;
