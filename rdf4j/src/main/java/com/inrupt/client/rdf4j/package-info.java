0c9c77f45b1043848d0375060bd90ad83cb4a14f
Timea Turdean <4144203+timea-solid@users.noreply.github.com>
2022-10-24 11:39:54 +0200

    docu for rdf4j usage on module level

--- rdf4j/src/main/java/com/inrupt/client/rdf4j/package-info.java
+++ rdf4j/src/main/java/com/inrupt/client/rdf4j/package-info.java
@@ -20,5 +20,55 @@
  */
 /**
  * <h2>RDF4J RDF support for the Inrupt client libraries.</h2>
+ *
+ * <p>The RDF4J module gives two possibilities to read RDF data: through the Processor {@link RDF4JRdfProcessor}
+ * and through the BodyHandler {@link RDF4JBodyHandlers}.
+ *
+ * <h3>Example of using the RDF4J Processor toDataset() method to read triples
+ * from a trig file into a {@code Dataset}:</h3>
+ *
+ * <pre>{@code
+    Rdf4JRdfProcessor processor = ServiceProvider.getRdfProcessor();
+    Dataset dataset;
+    try (InputStream input = Test.class.getResourceAsStream("/tripleExamples.trig")) {
+        dataset = processor.toDataset(Syntax.TRIG, input);
+    }
+    System.out.println("Number of triples in file: " + dataset.stream().count());
+ * }</pre>
+ *
+ * <h3>Example of using the RDF4J BodyHandler ofModel() method to read the triples
+ * from the same trig file into a {@code Model}:</h3>
+ * <pre>{@code
+    Request request = Request.newBuilder()
+        .uri(URI.create("https://example.example/tripleExamples.trig"))
+        .GET()
+        .build();
+
+    Response<Model> response = client.send(request, RDF4JBodyHandlers.ofModel());
+
+    System.out.println("HTTP status code: " + response.statusCode());
+    System.out.println("Number of triples in file: " + response.body().size());
+ * }</pre>
+ *
+ * <p>The {@link RDF4JBodyPublishers} can be used to write triples. An example that uses the
+ * POST method to write a {@code Model}:
+ *
+ * <pre>{@code
+    ModelBuilder builder = new ModelBuilder();
+    builder.namedGraph("https://example.example/graph")
+            .subject("https://example.example/subject")
+                .add("https://example.example/predicate", "object");
+    Model model = builder.build();
+
+    Request request = Request.newBuilder()
+            .uri("https://example.example/postEndpoint"))
+            .header("Content-Type", "text/turtle")
+            .POST(RDF4JBodyPublishers.ofModel(model))
+            .build();
+
+    Response<Void> response = client.send(request, Response.BodyHandlers.discarding());
+
+    System.out.println("HTTP status code: " + response.statusCode());
+ * }</pre>
  */
 package com.inrupt.client.rdf4j;
