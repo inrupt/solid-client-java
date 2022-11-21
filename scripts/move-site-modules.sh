0cc1997471a731b8810030c496e8ea81a5122368
Timea Turdean <4144203+timea-solid@users.noreply.github.com>
2022-11-21 10:31:01 +0100

    try to fix build on main

--- scripts/move-site-modules.sh
+++ scripts/move-site-modules.sh
@@ -2,5 +2,5 @@
 
 for MODULE in api bom core httpclient jackson jena jsonb okhttp openid parser rdf4j uma vc vocabulary webid
 do
-    mv target/$MODULE target/site/
+    mv target/$MODULE/site target/site/
 done
