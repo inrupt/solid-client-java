683503c8149a50828a632bcbde416a87de560683
Timea <4144203+timea-solid@users.noreply.github.com>
2022-12-09 15:00:57 +0100

    JCL-171: OpenId documentation (#154)
    * updated openID doc

--- openid/src/main/java/com/inrupt/client/openid/package-info.java
+++ openid/src/main/java/com/inrupt/client/openid/package-info.java
@@ -21,46 +21,114 @@
 /**
  * <h2>OpenID support for the Inrupt client libraries.</h2>
  *
- * <p>This modules uses the {@code OpenIdProvider} to interact with a OpenId Provider
+ * <p>This module uses the {@code OpenIdProvider} to interact with a OpenId Provider
  * as described in the <a href="https://solidproject.org/TR/oidc-primer"> Solid-OIDC Primer v 0.1.0</a>
  * 
- * <p>To retrieve the OpenId Provider configuration one can make use of the metadata() method as in the next example:
+ * <p>{@code OpenIdProvider} helps in the interaction with the token endpoint and to construct helper
+ * requests for authentication.
+ * 
+ * <h3>Discovering the openID configuration</h3>
  *
  * <pre>{@code
-    DPoP dpop = new DPoP();
-    HttpProcessor client = ServiceProvider.getHttpProcessor();
-    OpenIdProvider openIdProvider = new OpenIdProvider(URI.create("https:/example.example/issuer")), dpop, client);
+   Client client = ClientProvider.getClient();
+   OpenIdProvider openIdProvider = new OpenIdProvider(URI.create("https://issuer.example"), client));
 
-    Metadata opConfig = openIdProvider.metadata();
+   Metadata opConfig = openIdProvider.metadata();
 
-    System.out.println("Token endpoint is: " + opConfig.tokenEndpoint.toString());
-    System.out.println("Revocation endpoint is: " + opConfig.revocationEndpoint.toString());
+   System.out.println("Authorization endpoint is: " + opConfig.authorizationEndpoint.toString());
+   System.out.println("Token endpoint is: " + opConfig.tokenEndpoint.toString());
  * }</pre>
  * 
- * <p>Generating a PKCE code challenge and verifier is as simple as in the next example:
+ * <h3>Creating an AuthorizationRequest (needed for browser-based authorization code flow)</h3>
  * 
  * <pre>{@code
-    String challenge = PKCE.createChallenge("example verifier");
-    String verifier = PKCE.createVerifier();
+   Client client = ClientProvider.getClient();
+
+   AuthorizationRequest authReq = AuthorizationRequest.newBuilder()
+      .responseType("code") //required response_type
+      .scope(List.of("openid", "webid", "offline_access")) //required scope
+      .build(
+            "s6BhdRkqt3", //required client_id
+            URI.create("https://example.example/callback"); //required redirect_uri
+
+   Request authorizationRequest = Request.newBuilder()
+            .uri(URI.create(openIdProvider.authorize(authReq)))
+            .GET()
+            .build();
+
+   Response authorizationResponse = client.send(authorizationRequest, Response.BodyHandlers.ofString());
  * }</pre>
  * 
- * <p>At some point we need to request a token with a code and a verifier. This is achieved
- * as in the following example:
+ * <h3>Obtaining an ID token</h3>
  * 
  * <pre>{@code
-    TokenRequest tokenReq = TokenRequest.newBuilder()
-        .code("someCode")
-        .codeVerifier("myCodeverifier")
-        .build(
-            "authorization_code",
-            "myClientId",
-            URI.create("https://example.example/redirectUri")
-        );
+   com.inrupt.client.openid.TokenRequest tokenReq = TokenRequest.newBuilder()
+            .code("code")
+            .codeVerifier("myCodeverifier")
+            .redirectUri(URI.create("https://app.example/callback"))
+            .clientSecret("myClientSecret")
+            .authMethod("client_secret_basic")
+            .build(
+                "authorization_code",
+                "s6BhdRkqt3") //same client_id as from the authorization
+            );
+   com.inrupt.client.openid.TokenResponse token = openIdProvider.token(tokenReq);
 
-    TokenResponse token = openIdProvider.token(tokenReq);
-
-    System.out.println("Token id is: " + token.idToken);
-    System.out.println("Token type is: " + token.tokenType);
+   System.out.println("ID Token: " + token.idToken);
+   System.out.println("Token type: " + token.tokenType);
  * }</pre>
+ * 
+ * <h3>If we already have a session, we can use it with the ID Tokens serialized as signed
+ * JWTs. This abstraction can be used to make use of identity-based authorization
+ * in Solid.</h3>
+ *
+ * <pre>{@code
+   Client client = ClientProvider.getClient();
+   Session session = client.session(OpenIdSession.ofIdToken(jwt));
+   Response res = session.send(req, bodyHandler);
+ * }</pre>
+ *
+ * <p>A developer can configure aspects of the ID Token validation.
+ * All tokens require the presence of subject ({@code sub}) and issuer ({@code iss}) claims as well as
+ * issued at ({@code iat}) and expiration ({@code exp}) claims. By default, signature verification
+ * is not enabled, but it can be turned on via configuration, as can audience verification.
+ *
+ * <pre>{@code
+   Client client = ClientProvider.getClient();
+
+   OpenIdVerificationConfig config = new OpenIdVerificationConfig();
+   config.setExpectedAudience("https://app.example/id");
+   config.setPublicKeyLocation("https://issuer.example/jwks");
+   config.setExpGracePeriodSecs(60);
+
+   Session session = client.session(OpenIdSession.ofIdToken(jwt, config));
+
+   Response res = session.send(req, bodyHandler);
+ * }</pre>
+ * 
+ * <p>An invalid token will throw an {@link OpenIdException} during session creation.
+ * 
+ * <h3>Ending the session.</h3>
+ * 
+ * <pre>{@code
+   EndSessionRequest endReq = EndSessionRequest.Builder.newBuilder()
+         .postLogoutRedirectUri(URI.create("https://example.example/callback")) //redirect_uri
+         .clientId("s6BhdRkqt3") //client_id
+         .state("code")
+         .build();
+
+   URI uri = openIdProvider.endSession(endReq);
+ * }</pre>
+ * 
+ * <h3>Generating a PKCE code challenge and verifier.</h3>
+ * 
+ * <p>The default challenge encoding is SHA-256.
+ * Pass an algorithm to createChallenge if another encoding is required.
+ * 
+ * <pre>{@code
+   String verifier = PKCE.createVerifier();
+   String challenge = PKCE.createChallenge(verifier);
+ * }</pre>
+ * 
  */
 package com.inrupt.client.openid;
