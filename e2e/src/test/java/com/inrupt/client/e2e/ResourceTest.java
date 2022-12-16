1d3bc237f6d5fd629d7e7a9913750941d16bdf2d
Timea Turdean <4144203+timea-solid@users.noreply.github.com>
2022-12-16 12:20:16 +0100

    added auth scenarios & microProfile config

--- e2e/src/test/java/com/inrupt/client/e2e/ResourceTest.java
+++ e2e/src/test/java/com/inrupt/client/e2e/ResourceTest.java
@@ -20,73 +20,86 @@
  */
 package com.inrupt.client.e2e;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.jose4j.jwx.HeaderParameterNames.TYPE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.apache.jena.rdf.model.ResourceFactory.createResource;
 import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
 import static org.apache.jena.rdf.model.ResourceFactory.createStatement;
+import static org.apache.jena.rdf.model.ResourceFactory.createTypedLiteral;
 
-import com.apicatalog.jsonld.http.link.Link;
 import com.inrupt.client.Client;
 import com.inrupt.client.ClientProvider;
-import com.inrupt.client.Dataset;
 import com.inrupt.client.Headers;
 import com.inrupt.client.InruptClientException;
-import com.inrupt.client.Quad;
-import com.inrupt.client.RDFNode;
 import com.inrupt.client.Request;
 import com.inrupt.client.Response;
-import com.inrupt.client.Session;
-import com.inrupt.client.Syntax;
 import com.inrupt.client.jena.JenaBodyHandlers;
 import com.inrupt.client.jena.JenaBodyPublishers;
 import com.inrupt.client.openid.OpenIdSession;
-import com.inrupt.client.spi.HttpService;
-import com.inrupt.client.spi.RdfService;
-import com.inrupt.client.spi.ServiceProvider;
 import com.inrupt.client.vocabulary.LDP;
 import com.inrupt.client.webid.WebIdBodyHandlers;
 
+import io.smallrye.config.SmallRyeConfig;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.update.UpdateFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.jose4j.jwk.PublicJsonWebKey;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.lang.JoseException;
+import org.jose4j.lang.UncheckedJoseException;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ResourceTest {
     
-    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceTest.class);
+    private static Config config = ConfigProvider.getConfig();
+    private static SmallRyeConfig smallRyeConfig = config.unwrap(SmallRyeConfig.class);
     private static Client session = ClientProvider.getClient();
+
     private static String podUrl = "";
     private static String testResource = "";
 
     @BeforeAll
     static void setup() {
-        final var webid = URI.create((String) claim); 
-        //get authenticated session
-        session = session.session(OpenIdSession.ofIdToken(jwt.getRawToken()));
+        final var WEBID = URI.create(smallRyeConfig.getValue("webid", String.class));
+        final var SUB = smallRyeConfig.getValue("username", String.class);
+        final var ISS = smallRyeConfig.getValue("issuer", String.class);
+        final var AZP = smallRyeConfig.getValue("azp", String.class);
+        
+        //create a test claim
+        final Map<String, Object> claims = new HashMap<>();
+        claims.put("webid", WEBID.toString());
+        claims.put("sub", SUB);
+        claims.put("iss", ISS);
+        claims.put("azp", AZP);
 
-        //getStorage of webID
-        final var req = Request.newBuilder(webid).header("Accept", "text/turtle").GET().build();
-        final var profile = session.send(req, WebIdBodyHandlers.ofWebIdProfile(webid)).body();
+        final String token = generateIdToken(claims);
+        session = session.session(OpenIdSession.ofIdToken(token));
 
-        //either get it from the WebID or from the Link header of the resource
+        final var req = Request.newBuilder(WEBID).header("Accept", "text/turtle").GET().build();
+        final var profile = session.send(req, WebIdBodyHandlers.ofWebIdProfile(WEBID)).body();
+
         if (!profile.getStorage().isEmpty()) {
             podUrl = profile.getStorage().iterator().next().toString();
             if (!podUrl.endsWith("/"))
@@ -95,6 +108,7 @@ public class ResourceTest {
         }
     }
 
+    @Disabled("until proper token available")
     @Test
     @DisplayName("user creates a Solid resource and adds a boolean triple to it")
     void createResourceTest() {
@@ -122,18 +136,27 @@ public class ResourceTest {
             statementsToDelete = responseRdf.body().listStatements(createResource(newResource), createProperty(predicate), (org.apache.jena.rdf.model.RDFNode)null).toList();
         }
 
-        final List<Statement> statementsToAdd = List.of(createStatement(createResource(newResource), createProperty(predicate), ResourceFactory.createTypedLiteral(true)));
+        final List<Statement> statementsToAdd = List.of(createStatement(createResource(newResource), createProperty(predicate), createTypedLiteral(true)));
 
         final var ur = UpdateFactory.create(createDeleteInsertSparqlQuery(statementsToDelete, statementsToAdd));
         final Request requestPatch = Request.newBuilder(newResourceURL)
             .header("Content-Type", "application/sparql-update")
             .method("PATCH", JenaBodyPublishers.ofUpdateRequest(ur)).build();
         final var responsePatch = session.send(requestPatch, Response.BodyHandlers.discarding());
-        if (responsePatch.statusCode() != 204) {
-            throw new InruptClientException("Faild to create a new resource at " + newResource); //create appropriate exception
-        }
+        
+        assertEquals(204, responsePatch.statusCode());
+
+        final Request requestRdf = Request.newBuilder(newResourceURL).GET().build();
+        final var responseRdf = session.send(requestRdf, JenaBodyHandlers.ofModel());
+        final var insertedStatement = responseRdf
+                        .body().listStatements(createResource(newResource),
+                                createProperty(predicate), createTypedLiteral(true))
+                        .toList();
+        assertEquals(1, insertedStatement.size());
+        
     }
 
+    @Disabled("until proper token available")
     @Test
     @DisplayName("can create and remove Containers")
     void containerCreateDeleteTest() {
@@ -151,9 +174,9 @@ public class ResourceTest {
 
         final var responseCreate =
                 session.send(reqCreate, Response.BodyHandlers.discarding());
-        if (responseCreate.statusCode() != 204) {
-            throw new InruptClientException("Faild to create a new container at " + containerURL); //create appropriate exception
-        }
+        
+        assertEquals(204, responseCreate.statusCode());
+
         //TODO
         //consider creating contianer with slug
 
@@ -162,11 +185,11 @@ public class ResourceTest {
 
         final Response<Void> responseDelete =
                 session.send(reqDelete, Response.BodyHandlers.discarding());
-        if (responseDelete.statusCode() != 200) {
-            throw new InruptClientException("Faild to delete a container at " + containerURL); //create appropriate exception
-        }
+        
+        assertEquals(200, responseDelete.statusCode());
     }
     
+    @Disabled("until proper token available")
     @Test
     @DisplayName("can create, delete, and differentiate between RDF and non-RDF Resources")
     void rdfNonRdfTest() {
@@ -181,9 +204,8 @@ public class ResourceTest {
 
         final Response<Void> responseCreate =
                 session.send(reqCreate, Response.BodyHandlers.discarding());
-        if (responseCreate.statusCode() != 204) {
-            throw new InruptClientException("Faild to create the non RDF resource " + fileURL); //create appropriate exception
-        }
+        
+        assertEquals(204, responseCreate.statusCode());
 
         //TODO
         //test if file is a SolidResource or raw file
@@ -192,11 +214,11 @@ public class ResourceTest {
         final Request reqDelete = Request.newBuilder(URI.create(fileURL)).DELETE().build();
         final Response<String> responseDelete =
                 session.send(reqDelete, Response.BodyHandlers.ofString());
-        if (responseDelete.statusCode() != 200) {
-            throw new InruptClientException("Faild to delete a container at " + fileURL); //create appropriate exception
-        }
+        
+        assertEquals(200, responseDelete.statusCode());
     }
 
+    @Disabled("until proper token available")
     @Test
     @DisplayName("can update statements containing Blank Nodes in different instances of the same model")
     void blankNodesTest() {
@@ -214,9 +236,7 @@ public class ResourceTest {
                 .PUT(Request.BodyPublishers.noBody())
                 .build();
         final Response<Void> resp = session.send(requestCreateIfNotExist, Response.BodyHandlers.discarding());
-        if (resp.statusCode() != 204) {
-            throw new InruptClientException("Failed to create solid resource at " + newResource); 
-        }
+        assertEquals(204, resp.statusCode());
 
         //if the resource already exists -> we get all its statements and filter out the ones we are interested in
         List<Statement> statementsToDelete = new ArrayList<>();
@@ -237,20 +257,15 @@ public class ResourceTest {
             .method("PATCH", JenaBodyPublishers.ofUpdateRequest(ur)).build();
 
         final var responseCreate = session.send(requestCreate, Response.BodyHandlers.discarding());
-        if (responseCreate.statusCode() != 204) {
-            throw new InruptClientException("Faild to create a new resource at " + newResource); //create appropriate exception
-        }
         
-        //get the innitialised dataset and change the non blank node
+        assertEquals(204, responseCreate.statusCode());
+        
+        //get the newly created dataset and change the non blank node
         final Request req = Request.newBuilder(URI.create(testResource)).GET().build();
         final Response<Model> res = session.send(req, JenaBodyHandlers.ofModel());
-        List<Statement> statementsToDeleteAgain = new ArrayList<>();
-
-        if (res.statusCode() != 200) {
-            LOGGER.debug("Failed to retrieve dataset");
-        } else {
-            statementsToDeleteAgain = res.body().listStatements(createResource(newResource), createProperty(predicate), (org.apache.jena.rdf.model.RDFNode)null).toList();
-        }
+        
+        assertEquals(200, res.statusCode());
+        final List<Statement> statementsToDeleteAgain = res.body().listStatements(createResource(newResource), createProperty(predicate), (org.apache.jena.rdf.model.RDFNode)null).toList();
 
         final List<Statement> statementsToAddAgain = List.of(createStatement(createResource(newResource), createProperty(predicate), ResourceFactory.createTypedLiteral(false)));
 
@@ -261,9 +276,8 @@ public class ResourceTest {
             .method("PATCH", JenaBodyPublishers.ofUpdateRequest(ur2)).build();
 
         final var responseCreate2 = session.send(requestCreate2, Response.BodyHandlers.discarding());
-        if (responseCreate2.statusCode() != 204) {
-            throw new InruptClientException("Faild to update resource at " + newResource); //create appropriate exception
-        }
+        
+        assertEquals(204, responseCreate2.statusCode());
 
         //clean up -> delete all
     }
@@ -279,26 +293,55 @@ public class ResourceTest {
     }
 
     //utility method
-    private String createDeleteInsertSparqlQuery(List<Statement> quadsToDelete, List<Statement> quadsToAdd) {
+    private String createDeleteInsertSparqlQuery(List<Statement> quadsToDelete,
+            List<Statement> quadsToAdd) {
         //use a SparqlBuilder....
         var sparql = "";
         if (!quadsToDelete.isEmpty()) {
             sparql += "DELETE DATA { ";
-            sparql += quadsToDelete.stream().map(quad ->
-                "<" + quad.getSubject() + "> <" + quad.getPredicate() + "> "
-                + quad.getObject() + "'. ")
-            .collect(Collectors.joining());
-                sparql += " }; \n";
+            sparql +=
+                    quadsToDelete.stream()
+                            .map(quad -> "<" + quad.getSubject() + "> <" + quad.getPredicate()
+                                    + "> " + quad.getObject() + "'. ")
+                            .collect(Collectors.joining());
+            sparql += " }; \n";
         }
         if (!quadsToAdd.isEmpty()) {
             sparql += "INSERT DATA { ";
-            sparql += quadsToAdd.stream().map(quad -> 
-                "<" + quad.getSubject() + "> <" + quad.getPredicate() + "> "
-                + quad.getObject() + "'. ")
-            .collect(Collectors.joining());
+            sparql +=
+                    quadsToAdd.stream()
+                            .map(quad -> "<" + quad.getSubject() + "> <" + quad.getPredicate()
+                                    + "> " + quad.getObject() + "'. ")
+                            .collect(Collectors.joining());
             sparql += " }";
         }
         return sparql;
+    }
+    
+    static String generateIdToken(final Map<String, Object> claims) {
+        try (final InputStream resource = ResourceTest.class.getResourceAsStream("/signing-key.json")) {
+            final String jwks = IOUtils.toString(resource, UTF_8);
+            final PublicJsonWebKey jwk = PublicJsonWebKey.Factory
+                .newPublicJwk(jwks);
+
+            final JsonWebSignature jws = new JsonWebSignature();
+            jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+            jws.setHeader(TYPE, "JWT");
+            jws.setKey(jwk.getPrivateKey());
+            final JwtClaims jwtClaims = new JwtClaims();
+            jwtClaims.setJwtId(UUID.randomUUID().toString());
+            jwtClaims.setExpirationTimeMinutesInTheFuture(5);
+            jwtClaims.setIssuedAtToNow();
+            // override/set claims
+            claims.entrySet().forEach(entry -> jwtClaims.setClaim(entry.getKey(), entry.getValue()));
+            jws.setPayload(jwtClaims.toJson());
+
+            return jws.getCompactSerialization();
+        } catch (final IOException ex) {
+            throw new UncheckedIOException("Unable to read JWK", ex);
+        } catch (final JoseException ex) {
+            throw new UncheckedJoseException("Unable to generate DPoP token", ex);
+        }
     }
 }
 
