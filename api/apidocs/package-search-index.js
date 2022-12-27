bd388af651e54e5e04faa05f630a38362554bba3
acoburn <acoburn@users.noreply.github.com>
2022-12-27 15:43:20 +0000

    deploy: ede9adaeca88996ba81a40762c19d4edd730d4cd

--- api/apidocs/package-search-index.js
+++ api/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.inrupt.client"},{"l":"com.inrupt.client.rdf"},{"l":"com.inrupt.client.spi"},{"l":"com.inrupt.client.util"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.inrupt.client"},{"l":"com.inrupt.client.spi"},{"l":"com.inrupt.client.util"}]
