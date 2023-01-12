41727390725dc99fce68d73b0b143b7535e263a3
Ben Cummings <87279784+BnjmnCummings@users.noreply.github.com>
2023-01-12 15:58:14 +0000

    JCL-222: Rename quarkus module to webapp (#235)

--- examples/webapp/src/main/java/com/inrupt/client/examples/webapp/SolidStorage.java
+++ examples/webapp/src/main/java/com/inrupt/client/examples/webapp/SolidStorage.java
@@ -18,7 +18,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.inrupt.client.examples.quarkus;
+package com.inrupt.client.examples.webapp;
 
 import com.inrupt.client.openid.OpenIdSession;
 import com.inrupt.client.solid.SolidClient;
