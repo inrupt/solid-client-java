8a774c691bbbd28f3d807d5bd8b2bd1f1b67bfea
Timea Turdean <4144203+timea-solid@users.noreply.github.com>
2023-01-31 17:49:38 +0100

    new integration tests structure

---
+++ integration/parent/src/main/java/com/inrupt/client/integration/parent/ServerTest.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright 2023 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.inrupt.client.integration.parent;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.jose4j.jwx.HeaderParameterNames.TYPE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.inrupt.client.Headers.WwwAuthenticate;
+import com.inrupt.client.Request;
+import com.inrupt.client.Resource;
+import com.inrupt.client.Response;
+import com.inrupt.client.auth.Session;
+import com.inrupt.client.openid.OpenIdSession;
+import com.inrupt.client.solid.SolidResourceException;
+import com.inrupt.client.solid.SolidResourceHandlers;
+import com.inrupt.client.solid.SolidSyncClient;
+import com.inrupt.client.webid.WebIdProfile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.rdf.api.RDFSyntax;
+import org.jose4j.jwk.PublicJsonWebKey;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.lang.JoseException;
+import org.jose4j.lang.UncheckedJoseException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ServerTest {
+    private static MockSolidServer mockHttpServer;
+    private static MockOpenIDProvider identityProviderServer;
+    private static MockUMAAuthorizationServer authServer;
+    private static MockWebIdSevice webIdService;
+    private static String podUrl;
+    private static String issuer;
+    private static String webidUrl;
+    private static final String MOCK_USERNAME = "someuser";
+    private static final String PRIVATE_RESOURCE_PATH = "private";
+
+    @BeforeAll
+    static void setup() {
+        authServer = new MockUMAAuthorizationServer();
+        authServer.start();
+
+        mockHttpServer = new MockSolidServer(authServer.getMockServerUrl());
+        mockHttpServer.start();
+
+        identityProviderServer = new MockOpenIDProvider(MOCK_USERNAME);
+        identityProviderServer.start();
+
+        webIdService = new MockWebIdSevice(
+            mockHttpServer.getMockServerUrl(),
+            identityProviderServer.getMockServerUrl(),
+            MOCK_USERNAME);
+        webIdService.start();
+
+        State.PRIVATE_RESOURCE_PATH = PRIVATE_RESOURCE_PATH;
+
+        webidUrl = webIdService.getMockServerUrl() + "/" + MOCK_USERNAME;
+
+        State.WEBID = URI.create(webidUrl);
+        final SolidSyncClient client = SolidSyncClient.getClient();
+        try (final WebIdProfile profile = client.read(URI.create(webidUrl), WebIdProfile.class)) {
+            issuer = profile.getOidcIssuer().iterator().next().toString();
+            podUrl = profile.getStorage().iterator().next().toString();
+        }
+    }
+
+    @AfterAll
+    static void teardown() {
+        mockHttpServer.stop();
+        identityProviderServer.stop();
+        authServer.stop();
+        webIdService.stop();
+    }
+
+    @Test
+    void testAnonymousUserCRUD() {
+        //create an authenticated client
+        final SolidSyncClient client = SolidSyncClient.getClient().session(Session.anonymous());
+        //create a public resource
+        final var resourceUri = URI.create(podUrl + "/playlist");
+        final var playlist = new Playlist(resourceUri, null, null);
+
+        final var req =
+                Request.newBuilder(playlist.getIdentifier()).header(Utils.CONTENT_TYPE, Utils.TEXT_TURTLE)
+                        .header(Utils.IF_NONE_MATCH, Utils.WILDCARD).PUT(cast(playlist)).build();
+        final var res =
+                client.send(req, Response.BodyHandlers.discarding());
+
+        assertTrue(Utils.isSuccessful(res.statusCode()));
+
+        final var reqGet =
+                Request.newBuilder().uri(resourceUri).header(Utils.ACCEPT, Utils.TEXT_TURTLE).GET().build();
+        final var resGet = client.send(reqGet, SolidResourceHandlers.ofSolidResource());
+
+        assertTrue(Utils.isSuccessful(resGet.statusCode()));
+
+        final var reqDelete =
+                Request.newBuilder().uri(resourceUri).header(Utils.ACCEPT, Utils.TEXT_TURTLE).DELETE().build();
+        final var resDelete = client.send(reqDelete, Response.BodyHandlers.discarding());
+
+        assertTrue(Utils.isSuccessful(resDelete.statusCode()));
+    }
+
+    @Test
+    void test412() {
+        //create an authenticated client
+        final SolidSyncClient client = SolidSyncClient.getClient().session(Session.anonymous());
+        //create a public resource
+        final var resourceUri = URI.create(podUrl + "/playlist");
+        final var playlist = new Playlist(resourceUri, null, null);
+
+        final var req = Request.newBuilder(playlist.getIdentifier())
+                .header(Utils.CONTENT_TYPE, Utils.TEXT_TURTLE)
+                .header(Utils.IF_NONE_MATCH, Utils.WILDCARD).PUT(cast(playlist)).build();
+        final var res = client.send(req, Response.BodyHandlers.discarding());
+
+        assertTrue(Utils.isSuccessful(res.statusCode()));
+
+        //we try to create the exact same resource again
+        final var reqPut = Request.newBuilder(playlist.getIdentifier())
+                .header(Utils.CONTENT_TYPE, Utils.TEXT_TURTLE)
+                .header(Utils.IF_NONE_MATCH, Utils.WILDCARD).PUT(cast(playlist)).build();
+        final var resPut = client.send(reqPut, Response.BodyHandlers.discarding());
+
+        assertEquals(Utils.PRECONDITION_FAILED, resPut.statusCode());
+
+        final var reqDelete =
+                Request.newBuilder().uri(resourceUri).header(Utils.ACCEPT, Utils.TEXT_TURTLE).DELETE().build();
+        final var resDelete = client.send(reqDelete, Response.BodyHandlers.discarding());
+
+        assertTrue(Utils.isSuccessful(resDelete.statusCode()));
+    }
+
+    @Test
+    void testUnuthenticatedCRUD() {
+        //create an authenticated client
+        final SolidSyncClient client = SolidSyncClient.getClient();
+        //create a private resource
+        final var resourceUri =
+                URI.create(podUrl + "/" + State.PRIVATE_RESOURCE_PATH + "/playlist");
+        final var playlist = new Playlist(resourceUri, null, null);
+        final var req = Request.newBuilder(playlist.getIdentifier())
+                .header(Utils.CONTENT_TYPE, Utils.TEXT_TURTLE)
+                .header(Utils.IF_NONE_MATCH, Utils.WILDCARD).PUT(cast(playlist)).build();
+        final var res = client.send(req, Response.BodyHandlers.discarding());
+
+        assertEquals(Utils.UNAUTHORIZED, res.statusCode());
+
+        final var reqGet = Request.newBuilder().uri(resourceUri)
+                .header(Utils.ACCEPT, Utils.TEXT_TURTLE).GET().build();
+        final var resGet = client.send(reqGet, SolidResourceHandlers.ofSolidResource());
+
+        assertEquals(Utils.UNAUTHORIZED, resGet.statusCode());
+        final var challenges = WwwAuthenticate.parse(
+                    resGet.headers().firstValue("WWW-Authenticate").get())
+                .getChallenges();
+        assertTrue(challenges.toString().contains("Bearer"));
+
+        final var reqDelete = Request.newBuilder().uri(resourceUri)
+                .header(Utils.ACCEPT, Utils.TEXT_TURTLE).DELETE().build();
+        final var resDelete = client.send(reqDelete, Response.BodyHandlers.discarding());
+
+        assertEquals(Utils.UNAUTHORIZED, resDelete.statusCode());
+    }
+
+    @Test
+    void testAuthenticatedBearerCRUD() {
+        //authenticate with Bearer token
+        final var session = OpenIdSession.ofIdToken(setupIdToken(webidUrl, MOCK_USERNAME, issuer));
+        final SolidSyncClient client = SolidSyncClient.getClient().session(session);
+        //create a private resource
+        final var resourceUri =
+                URI.create(podUrl + "/" + State.PRIVATE_RESOURCE_PATH + "/playlist");
+        final var playlist = new Playlist(resourceUri, null, null);
+        final var req = Request.newBuilder(playlist.getIdentifier())
+                .header(Utils.CONTENT_TYPE, Utils.TEXT_TURTLE)
+                .header(Utils.IF_NONE_MATCH, Utils.WILDCARD).PUT(cast(playlist)).build();
+        final var res = client.send(req, Response.BodyHandlers.discarding());
+
+        assertTrue(Utils.isSuccessful(res.statusCode()));
+
+        final var reqGet = Request.newBuilder().uri(resourceUri)
+                .header(Utils.ACCEPT, Utils.TEXT_TURTLE).GET().build();
+        final var resGet = client.send(reqGet, SolidResourceHandlers.ofSolidResource());
+
+        assertTrue(Utils.isSuccessful(resGet.statusCode()));
+
+        final var reqDelete = Request.newBuilder().uri(resourceUri)
+                .header(Utils.ACCEPT, Utils.TEXT_TURTLE).DELETE().build();
+
+        final var resDelete = client.send(reqDelete, Response.BodyHandlers.discarding());
+
+        assertTrue(Utils.isSuccessful(resDelete.statusCode()));
+    }
+
+    private Request.BodyPublisher cast(final Resource resource) {
+        return com.inrupt.client.util.IOUtils.buffer(out -> {
+            try {
+                resource.serialize(RDFSyntax.TURTLE, out);
+            } catch (final IOException ex) {
+                throw new SolidResourceException("Unable to serialize " + resource.getClass().getName() +
+                        " into Solid Resource", ex);
+            }
+        });
+    }
+
+    static String setupIdToken(final String webid, final String username, final String issuer) {
+        final Map<String, Object> claims = new HashMap<>();
+        claims.put("webid", webid);
+        claims.put("sub", username);
+        claims.put("iss", issuer);
+        claims.put("azp", State.AZP);
+
+        return generateIdToken(claims);
+    }
+
+    static String generateIdToken(final Map<String, Object> claims) {
+        try (final InputStream resource = Utils.class.getResourceAsStream("/signing-key.json")) {
+            final String jwks = IOUtils.toString(resource, UTF_8);
+            final PublicJsonWebKey jwk = PublicJsonWebKey.Factory
+                .newPublicJwk(jwks);
+
+            final JsonWebSignature jws = new JsonWebSignature();
+            jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+            jws.setHeader(TYPE, "JWT");
+            jws.setKey(jwk.getPrivateKey());
+            final JwtClaims jwtClaims = new JwtClaims();
+            jwtClaims.setJwtId(UUID.randomUUID().toString());
+            jwtClaims.setExpirationTimeMinutesInTheFuture(5);
+            jwtClaims.setIssuedAtToNow();
+            // override/set claims
+            claims.entrySet().forEach(entry -> jwtClaims.setClaim(entry.getKey(), entry.getValue()));
+            jws.setPayload(jwtClaims.toJson());
+
+            return jws.getCompactSerialization();
+        } catch (final IOException ex) {
+            throw new UncheckedIOException("Unable to read JWK", ex);
+        } catch (final JoseException ex) {
+            throw new UncheckedJoseException("Unable to generate DPoP token", ex);
+        }
+    }
+}
---
+++ integration/parent/src/main/java/com/inrupt/client/integration/parent/SolidServerTransformer.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2023 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.inrupt.client.integration.parent;
+
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformer;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.RequestMethod;
+import com.github.tomakehurst.wiremock.http.ResponseDefinition;
+import com.inrupt.client.Headers;
+import com.inrupt.client.integration.parent.MockSolidServer.ServerBody;
+import com.inrupt.client.vocabulary.PIM;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+
+public class SolidServerTransformer extends ResponseDefinitionTransformer {
+
+    private final Map<String, ServerBody> storage;
+    private String asUri;
+
+    public SolidServerTransformer(final Map<String, ServerBody> storage, final String asUri) {
+        this.storage = storage;
+        this.asUri = asUri;
+    }
+
+    @Override
+    public String getName() {
+        return "Solid Server";
+    }
+
+    @Override
+    public ResponseDefinition transform(final Request request, final ResponseDefinition responseDefinition,
+            final FileSource files, final Parameters parameters) {
+
+        final var res = new ResponseDefinitionBuilder();
+
+        //determine if authenticated on private resouces
+        if (Utils.isPrivateResource(request.getUrl()) &&
+            request.getHeader("Authorization") == null) {
+            res.withHeader("WWW-Authenticate",
+                        "Bearer, DPoP algs=\"ES256\", UMA ticket=token-67890, as_uri=\""
+                        + this.asUri + "\"");
+            res.withStatus(Utils.UNAUTHORIZED);
+            return res.build();
+        }
+
+        if (request.getMethod().isOneOf(RequestMethod.GET, RequestMethod.HEAD)) {
+            if (this.storage.containsKey(request.getUrl())) {
+                final var serverBody = this.storage.get(request.getUrl());
+                res.withStatus(Utils.SUCCESS)
+                    .withHeader(Utils.CONTENT_TYPE, serverBody.contentType)
+                    .withBody(serverBody.body);
+
+                if (Utils.isPodRoot(request.getUrl())) {
+                    //we assume the root is publicly accessible
+                    res.withHeader("Link", Headers.Link
+                            .of(URI.create(PIM.getNamespace() + "Storage"), "type").toString());
+                }
+                return res.build();
+            } else {
+                return res.withStatus(Utils.NOT_FOUND).build();
+            }
+        }
+
+        if (request.getMethod().isOneOf(RequestMethod.POST)) {
+            if (request.getUrl().endsWith(Utils.FOLDER_SEPARATOR)) {
+                if (!this.storage.containsKey(request.getUrl())) {
+                    final String slug = request.getHeader("Slug");
+                    final String location = request.getUrl() + (slug != null ? slug : UUID.randomUUID());
+                    final boolean exists = this.storage.containsKey(request.getUrl());
+                    if (!Utils.WILDCARD.equals(request.getHeader(Utils.IF_NONE_MATCH)) || !exists) {
+                        this.storage.put(request.getUrl(), new ServerBody(request.getBody(),
+                                request.contentTypeHeader().mimeTypePart()));
+                        addSubContainersToStorage(request.getUrl(), request.contentTypeHeader().mimeTypePart());
+                        res.withStatus(Utils.CREATED);
+                        res.withHeader("Location", location);
+                    } else {
+                        res.withStatus(Utils.PRECONDITION_FAILED);
+                    }
+                } else {
+                    res.withStatus(Utils.NOT_FOUND);
+                }
+            } else {
+                res.withStatus(Utils.NOT_ALLOWED);
+            }
+            return res.build();
+        }
+
+        if (request.getMethod().isOneOf(RequestMethod.PUT)) {
+            final boolean exists = this.storage.containsKey(request.getUrl());
+            if (!Utils.WILDCARD.equals(request.getHeader(Utils.IF_NONE_MATCH)) || !exists) {
+                this.storage.put(request.getUrl(), new ServerBody(request.getBody(),
+                        request.contentTypeHeader().mimeTypePart()));
+                addSubContainersToStorage(request.getUrl(), request.contentTypeHeader().mimeTypePart());
+                res.withStatus(Utils.NO_CONTENT);
+            } else {
+                res.withStatus(Utils.PRECONDITION_FAILED);
+            }
+            return res.build();
+        }
+
+        if (request.getMethod().isOneOf(RequestMethod.PATCH)) {
+            if (request.contentTypeHeader().containsValue(Utils.SPARQL_UPDATE)) {
+                final var serverBody = this.storage.get(request.getUrl());
+                final var body = serverBody != null ? serverBody.body : new byte[0];
+                try {
+                    final byte[] newBody =
+                            Utils.modifyBody(body, request.getBodyAsString());
+                    this.storage.put(request.getUrl(),
+                            new ServerBody(newBody, serverBody.contentType));
+                    addSubContainersToStorage(request.getUrl(), request.contentTypeHeader().mimeTypePart());
+                    res.withStatus(Utils.NO_CONTENT);
+                } catch (IOException e) {
+                    res.withStatus(Utils.ERROR);
+                }
+            } else {
+                res.withStatus(Utils.ERROR);
+            }
+            return res.build();
+        }
+
+        if (request.getMethod().isOneOf(RequestMethod.DELETE)) {
+            if (this.storage.containsKey(request.getUrl())) {
+                this.storage.remove(request.getUrl());
+                res.withStatus(Utils.NO_CONTENT);
+            } else {
+                res.withStatus(Utils.NOT_FOUND);
+            }
+            return res.build();
+        }
+
+        return res.withStatus(Utils.NOT_ALLOWED).build();
+    }
+
+    private void addSubContainersToStorage(final String path, final String mimeType) {
+        final var newPath = path.startsWith("/") ? path.substring(1, path.length()) : path;
+        var containers = newPath.split("/");
+        while (containers.length > 0) {
+            final var uri = "/" + String.join("/", containers);
+            if (!this.storage.containsKey(uri)) {
+                this.storage.put(uri, new ServerBody(new byte[0], mimeType));
+            }
+            containers = Arrays.copyOf(containers, containers.length - 1);
+        }
+    }
+
+}
