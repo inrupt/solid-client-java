f679d88ea89560bb18486cad8ab7a8b444076c04
acoburn <acoburn@users.noreply.github.com>
2023-02-07 13:48:12 +0000

    deploy: ffa64262521082bab183f56172240c741f1fcead

---
+++ 1.0.0-SNAPSHOT/integration/base/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.inrupt.client.integration.base","l":"AuthenticationScenarios"},{"p":"com.inrupt.client.integration.base","l":"CoreModulesResource"},{"p":"com.inrupt.client.integration.base","l":"DomainModulesResource"}]
