3693e67d4b47e7df09d89d61cb97557d1a92476e
Aaron Coburn <aaronc@inrupt.com>
2023-02-09 14:45:14 -0500

    Test GH environment values

---
+++ api/src/test/java/com/inrupt/client/EnvTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.inrupt.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class EnvTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnvTest.class);
+
+    @Test
+    void testEnv() {
+        final Config conf = ConfigProvider.getConfig();
+        final URI webid = conf.getOptionalValue("inrupt.test.webid", URI.class).orElse(null);
+
+        LOGGER.info("WebID: [{}]", webid);
+        assertNotNull(webid);
+    }
+}
