0bba67edf6b3fe1f4b14989c80cad76dc4f7539c
Aaron Coburn <aaronc@inrupt.com>
2023-03-01 08:09:54 -0500

    JCL-73: Simplify project setup as a step to simplifying the docs (#318)

---
+++ archetypes/java/src/main/resources/archetype-resources/src/main/java/SolidApp.java
@@ -0,0 +1,42 @@
+/**
+ * A Solid application.
+ */
+package ${package};
+
+import com.inrupt.client.Resource;
+import com.inrupt.client.auth.Session;
+import com.inrupt.client.openid.OpenIdSession;
+import com.inrupt.client.solid.SolidContainer;
+import com.inrupt.client.solid.SolidSyncClient;
+import com.inrupt.client.webid.WebIdProfile;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolidApp {
+
+    protected Session getSession() {
+        return OpenIdSession.ofClientCredentials(URI.create("https://ISSUER-URL"),
+                "CLIENT_ID VALUE", "CLIENT_SECRET VALUE", "client_secret_basic");
+    }
+
+    public List<URI> readStorage() {
+        final var session = getSession();
+        final var client = SolidSyncClient.getClient().session(session);
+        final var resources = new ArrayList<URI>();
+
+        session.getPrincipal().ifPresent(webid -> {
+            try (final var profile = client.read(webid, WebIdProfile.class)) {
+                profile.getStorage().stream().findFirst().ifPresent(storage -> {
+                    try (final var container = client.read(storage, SolidContainer.class);
+                            final var stream = container.getContainedResources()) {
+                        stream.map(Resource::getIdentifier).forEach(resources::add);
+                    }
+                });
+            }
+        });
+
+        return resources;
+    }
+}
