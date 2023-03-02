004ce69ab140857afc6444bf341ef54b6dd3ba08
BnjmnCummings <ben.cummings@inrupt.com>
2023-03-02 16:06:14 +0000

    progress on jena use

--- examples/springboot/src/main/java/com/inrupt/client/examples/springboot/SolidController.java
+++ examples/springboot/src/main/java/com/inrupt/client/examples/springboot/SolidController.java
@@ -23,16 +23,24 @@ package com.inrupt.client.examples.springboot;
 
 import com.inrupt.client.Request;
 import com.inrupt.client.Response;
+import com.inrupt.client.jena.JenaBodyPublishers;
 import com.inrupt.client.openid.OpenIdSession;
 import com.inrupt.client.solid.SolidContainer;
 import com.inrupt.client.solid.SolidResource;
 import com.inrupt.client.solid.SolidSyncClient;
 import com.inrupt.client.webid.WebIdProfile;
+import com.inrupt.client.vocabulary.LDP;
+import com.inrupt.client.vocabulary.RDF;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,6 +49,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SolidController {
+    final Model model = ModelFactory.createDefaultModel();
+
+    private final Property P_TYPE = model.createProperty(RDF.type.toString());
+    private final Literal O_RESOURCE = model.createLiteral(LDP.Resource.toString());
+    private final Literal O_BASIC_CONTAINER = model.createLiteral(LDP.BasicContainer.toString());
+    
 
     final SolidSyncClient client = SolidSyncClient.getClient();
 
@@ -54,7 +68,7 @@ public class SolidController {
         return principal.getIdToken().getTokenValue();
     }
 
-    @GetMapping("/solid")
+    @GetMapping("/readPod")
     public List<URI> readFromPod(final @AuthenticationPrincipal OidcUser principal) {
         final var list = new ArrayList<URI>();
         final URI webid = URI.create(principal.getClaimAsString("webid"));
@@ -72,7 +86,7 @@ public class SolidController {
         return list;
     }
 
-    @GetMapping("/solid2")
+    /* @GetMapping("/writePod")//TODO make POST mapping
     public void writeToPod(final @AuthenticationPrincipal OidcUser principal) {
         final URI webid = URI.create(principal.getClaimAsString("webid"));
         final SolidSyncClient session =
@@ -82,6 +96,27 @@ public class SolidController {
                 final Request request = Request.newBuilder().uri(storage)
                         .header("Accept", "text/turtle").header("Slug", "test_resource")
                         .POST(Request.BodyPublishers.ofString("message")).build();
+
+                session.send(request, Response.BodyHandlers.discarding());
+            });
+        }
+    } */
+    
+    @GetMapping("/writePod")
+    public void writeToPod(final @AuthenticationPrincipal OidcUser principal) {
+        final URI webid = URI.create(principal.getClaimAsString("webid"));
+        final SolidSyncClient session =
+                client.session(OpenIdSession.ofIdToken(principal.getIdToken().getTokenValue()));
+        try (final WebIdProfile profile = session.read(webid, WebIdProfile.class)) {
+            profile.getStorage().stream().findFirst().ifPresent(storage -> {
+                final String RESOURCE_NAME = "test_container";
+                final Resource resource = model.createResource(storage+ RESOURCE_NAME);
+                //model.add(resource, P_TYPE, O_RESOURCE);
+                model.add(resource, P_TYPE, O_BASIC_CONTAINER);
+                
+                final Request request = Request.newBuilder().uri(storage)
+                        .header("Accept", "text/turtle").header("Slug", RESOURCE_NAME)
+                        .POST(JenaBodyPublishers.ofModel(model)).build();
 
                 session.send(request, Response.BodyHandlers.discarding());
             });
