b1d791d236dc79dcf41be85958f392a1578e7167
timea-solid <timea-solid@users.noreply.github.com>
2023-03-09 17:16:04 +0000

    deploy: 64a695b0ef53f2c6f24ee833809529aeea7495a3

---
+++ 1.0.0.Alpha7/vocabulary/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.inrupt.client.vocabulary","l":"LDP"},{"p":"com.inrupt.client.vocabulary","l":"PIM"},{"p":"com.inrupt.client.vocabulary","l":"RDF"},{"p":"com.inrupt.client.vocabulary","l":"RDFS"},{"p":"com.inrupt.client.vocabulary","l":"Solid"}]
