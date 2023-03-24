71cb60232b6058e58cf3f390c5394b4a348bc685
Timea Turdean <4144203+timea-solid@users.noreply.github.com>
2023-03-24 13:21:21 +0100

    added user agent info to e2e mock server

--- integration/base/src/main/java/com/inrupt/client/integration/base/MockSolidServer.java
+++ integration/base/src/main/java/com/inrupt/client/integration/base/MockSolidServer.java
@@ -24,13 +24,15 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-
+import com.inrupt.client.Request;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 class MockSolidServer {
 
     private final WireMockServer wireMockServer;
+    private static final String USER_AGENT = "InruptJavaClient/" + Request.class
+        .getPackage().getImplementationVersion();
     private Map<String, ServerBody> storage = new ConcurrentHashMap<>();
     private String asUri;
 
@@ -43,11 +45,11 @@ class MockSolidServer {
     }
 
     private void setupMocks() {
-        wireMockServer.stubFor(get(anyUrl()));
-        wireMockServer.stubFor(put(anyUrl()));
-        wireMockServer.stubFor(post(anyUrl()));
-        wireMockServer.stubFor(patch(anyUrl()));
-        wireMockServer.stubFor(delete(anyUrl()));
+        wireMockServer.stubFor(get(anyUrl()).withHeader("User-Agent", equalTo(USER_AGENT)));
+        wireMockServer.stubFor(put(anyUrl()).withHeader("User-Agent", equalTo(USER_AGENT)));
+        wireMockServer.stubFor(post(anyUrl()).withHeader("User-Agent", equalTo(USER_AGENT)));
+        wireMockServer.stubFor(patch(anyUrl()).withHeader("User-Agent", equalTo(USER_AGENT)));
+        wireMockServer.stubFor(delete(anyUrl()).withHeader("User-Agent", equalTo(USER_AGENT)));
     }
 
     public void start() {
