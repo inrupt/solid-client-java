d997971f84e62682a0234ce0701058dba2874d38
Aaron Coburn <aaronc@inrupt.com>
2023-05-25 11:54:26 -0400

    JCL-359: Reduce duplication in AccessCredential types (#484)

--- access-grant/src/main/java/com/inrupt/client/accessgrant/AccessCredential.java
+++ access-grant/src/main/java/com/inrupt/client/accessgrant/AccessCredential.java
@@ -20,87 +20,315 @@
  */
 package com.inrupt.client.accessgrant;
 
+import static com.inrupt.client.accessgrant.Utils.*;
+
 import java.net.URI;
 import java.time.Instant;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
-public interface AccessCredential {
+/** A base class for access credentials. **/
+public class AccessCredential {
+
+    protected static final String TYPE = "type";
+    protected static final String REVOCATION_LIST_2020_STATUS = "RevocationList2020Status";
+
+    private final String credential;
+    private final URI issuer;
+    private final URI identifier;
+    private final Set<String> types;
+    private final Set<String> purposes;
+    private final Set<String> modes;
+    private final Set<URI> resources;
+    private final URI recipient;
+    private final URI creator;
+    private final Instant expiration;
+    private final Status status;
+
+    /**
+     * Create a base class for credential types.
+     * @param identifier the credential identifier
+     * @param credential the full credential
+     * @param data items pertaining to user-defined credential data
+     * @param metadata items pertaining to server-managed credential data
+     */
+    protected AccessCredential(final URI identifier, final String credential,
+            final CredentialData data, final CredentialMetadata metadata) {
+        this.identifier = Objects.requireNonNull(identifier, "identifier may not be null");
+        this.credential = Objects.requireNonNull(credential, "credential may not be null!");
+        Objects.requireNonNull(data, "credential data may not be null!");
+        Objects.requireNonNull(metadata, "credential metadata may not be null!");
+
+        this.purposes = data.getPurposes();
+        this.resources = data.getResources();
+        this.modes = data.getModes();
+        this.recipient = data.getRecipient();
+
+        this.issuer = metadata.getIssuer();
+        this.creator = metadata.getCreator();
+        this.types = metadata.getTypes();
+        this.status = metadata.getStatus();
+        this.expiration = metadata.getExpiration();
+    }
 
     /**
      * Get the types of the access credential.
      *
      * @return the access credential types
      */
-    Set<String> getTypes();
+    public Set<String> getTypes() {
+        return types;
+    }
 
     /**
      * Get the access modes of the access credential.
      *
      * @return the access credential types
      */
-    Set<String> getModes();
+    public Set<String> getModes() {
+        return modes;
+    }
 
     /**
      * Get the revocation status of the access credential.
      *
      * @return the revocation status, if present
      */
-    Optional<Status> getStatus();
+    public Optional<Status> getStatus() {
+        return Optional.ofNullable(status);
+    }
 
     /**
      * Get the expiration time of the access credential.
      *
      * @return the expiration time
      */
-    Instant getExpiration();
+    public Instant getExpiration() {
+        return expiration != null ? expiration : Instant.MAX;
+    }
 
     /**
      * Get the issuer of the access credential.
      *
      * @return the issuer
      */
-    URI getIssuer();
+    public URI getIssuer() {
+        return issuer;
+    }
 
     /**
      * Get the identifier of the access credential.
      *
      * @return the identifier
      */
-    URI getIdentifier();
+    public URI getIdentifier() {
+        return identifier;
+    }
 
     /**
      * Get the collection of purposes associated with the access credential.
      *
      * @return the purposes
      */
-    Set<String> getPurposes();
+    public Set<String> getPurposes() {
+        return purposes;
+    }
 
     /**
      * Get the resources associated with the access credential.
      *
      * @return the associated resource identifiers
      */
-    Set<URI> getResources();
+    public Set<URI> getResources() {
+        return resources;
+    }
 
     /**
      * Get the creator of this access credential.
      *
      * @return the creator
      */
-    URI getCreator();
+    public URI getCreator() {
+        return creator;
+    }
 
     /**
      * Get the recipient of this access credential.
      *
      * @return the recipient, if present
      */
-    Optional<URI> getRecipient();
+    public Optional<URI> getRecipient() {
+        return Optional.ofNullable(recipient);
+    }
 
     /**
      * Serialize this access credential as a String.
      *
      * @return a serialized form of the credential
      */
-    String serialize();
+    public String serialize() {
+        return credential;
+    }
+
+    /** Server-managed credential data. */
+    public static class CredentialMetadata {
+        private final URI issuer;
+        private final URI creator;
+        private final Set<String> types;
+        private final Status status;
+        private final Instant expiration;
+
+        /**
+         * A collection of server-managed credential metadata.
+         *
+         * @param issuer the issuer
+         * @param creator the agent who created the credential
+         * @param types the credential types
+         * @param expiration the credential expiration
+         * @param status the credential status
+         */
+        public CredentialMetadata(final URI issuer, final URI creator, final Set<String> types,
+                final Instant expiration, final Status status) {
+            this.issuer = Objects.requireNonNull(issuer, "issuer may not be null!");
+            this.creator = Objects.requireNonNull(creator, "creator may not be null!");
+            this.types = Objects.requireNonNull(types, "types may not be null!");
+            this.expiration = expiration;
+            this.status = status;
+        }
+
+        /**
+         * Get the issuer of the credential.
+         *
+         * @return the issuer
+         */
+        public URI getIssuer() {
+            return issuer;
+        }
+
+        /**
+         * Get the creator of the credential.
+         *
+         * @return the creator
+         */
+        public URI getCreator() {
+            return creator;
+        }
+
+        /**
+         * Get the types of the credential.
+         *
+         * @return the credential types
+         */
+        public Set<String> getTypes() {
+            return types;
+        }
+
+        /**
+         * Get the expiration time of the credential.
+         *
+         * @return the expiration time
+         */
+        public Instant getExpiration() {
+            return expiration;
+        }
+
+        /**
+         * Get the status of the credential.
+         *
+         * @return the credential status
+         */
+        public Status getStatus() {
+            return status;
+        }
+    }
+
+    /**  User-managed credential data. */
+    public static class CredentialData {
+        private final Set<String> purposes;
+        private final Set<String> modes;
+        private final Set<URI> resources;
+        private final URI recipient;
+
+        /**
+         * Create a collection of user-managed credential data.
+         *
+         * @param resources the resources referenced by the credential
+         * @param modes the access modes defined by this credential
+         * @param purposes the purposes associated with this credential
+         * @param recipient the recipient for this credential, may be {@code null}
+         */
+        public CredentialData(final Set<URI> resources, final Set<String> modes,
+                final Set<String> purposes, final URI recipient) {
+            this.modes = Objects.requireNonNull(modes, "modes may not be null!");
+            this.purposes = Objects.requireNonNull(purposes, "purposes may not be null!");
+            this.resources = Objects.requireNonNull(resources, "resources may not be null!");
+            this.recipient = recipient;
+        }
+
+        /**
+         * Get the purposes associated with the credential.
+         *
+         * @return the purpose definitions
+         */
+        public Set<String> getPurposes() {
+            return purposes;
+        }
+
+        /**
+         * Get the access modes associated with the credential.
+         *
+         * @return the access modes
+         */
+        public Set<String> getModes() {
+            return modes;
+        }
+
+        /**
+         * Get the resource URIs associated with this credential.
+         *
+         * @return the resource URIs
+         */
+        public Set<URI> getResources() {
+            return resources;
+        }
+
+        /**
+         * Get the recipient associated with this credential.
+         *
+         * @return the credential, may be {@code null}
+         */
+        public URI getRecipient() {
+            return recipient;
+        }
+    }
+
+    static CredentialMetadata extractMetadata(final Map data) {
+        final URI issuer = asUri(data.get("issuer")).orElseThrow(() ->
+                new IllegalArgumentException("Missing or invalid issuer field"));
+        final Set<String> types = asSet(data.get(TYPE)).orElseGet(Collections::emptySet);
+        final Instant expiration = asInstant(data.get("expirationDate")).orElse(Instant.MAX);
+        final Status status = asMap(data.get("credentialStatus")).flatMap(credentialStatus ->
+                asSet(credentialStatus.get(TYPE)).filter(statusTypes ->
+                    statusTypes.contains(REVOCATION_LIST_2020_STATUS)).map(x ->
+                        asRevocationList2020(credentialStatus))).orElse(null);
+
+        final Map subject = asMap(data.get("credentialSubject")).orElseThrow(() ->
+                new IllegalArgumentException("Missing or invalid credentialSubject field"));
+
+        final URI creator = asUri(subject.get("id")).orElseThrow(() ->
+                new IllegalArgumentException("Missing or invalid credentialSubject.id field"));
+
+        return new CredentialMetadata(issuer, creator, types, expiration, status);
+    }
+
+    static Map extractConsent(final Map data, final String property) {
+        final Map subject = asMap(data.get("credentialSubject")).orElseThrow(() ->
+                new IllegalArgumentException("Missing or invalid credentialSubject field"));
+        return asMap(subject.get(property)).orElseThrow(() ->
+                // Unsupported structure
+                new IllegalArgumentException("Invalid Access Request: missing consent clause"));
+    }
 }
--- access-grant/src/main/java/com/inrupt/client/accessgrant/AccessRequest.java
+++ access-grant/src/main/java/com/inrupt/client/accessgrant/AccessRequest.java
@@ -30,13 +30,11 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.time.Instant;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -45,80 +43,20 @@ import org.apache.commons.io.IOUtils;
 /**
  * An Access Request abstraction, for use when interacting with Solid resources.
  */
-public class AccessRequest implements AccessCredential {
+public class AccessRequest extends AccessCredential {
 
-    private static final String TYPE = "type";
-    private static final String REVOCATION_LIST_2020_STATUS = "RevocationList2020Status";
     private static final JsonService jsonService = ServiceProvider.getJsonService();
     private static final Set<String> supportedTypes = getSupportedTypes();
-
-    private final String credential;
-    private final URI issuer;
-    private final URI identifier;
-    private final Set<String> types;
-    private final Set<String> purposes;
-    private final Set<String> modes;
-    private final Set<URI> resources;
-    private final URI recipient;
-    private final URI creator;
-    private final Instant expiration;
-    private final Status status;
 
     /**
      * Read a verifiable presentation as an AccessRequest.
      *
      * @param serialization the serialized form of an Access Request
      */
-    protected AccessRequest(final String serialization) throws IOException {
-        try (final InputStream in = new ByteArrayInputStream(serialization.getBytes())) {
-            // TODO process as JSON-LD
-            final Map<String, Object> data = jsonService.fromJson(in,
-                    new HashMap<String, Object>(){}.getClass().getGenericSuperclass());
-
-            final List<Map> vcs = getCredentialsFromPresentation(data, supportedTypes);
-            if (vcs.size() != 1) {
-                throw new IllegalArgumentException(
-                        "Invalid Access Request: ambiguous number of verifiable credentials");
-            }
-            final Map vc = vcs.get(0);
-
-            if (asSet(data.get(TYPE)).orElseGet(Collections::emptySet).contains("VerifiablePresentation")) {
-                this.credential = serialization;
-                this.issuer = asUri(vc.get("issuer")).orElseThrow(() ->
-                        new IllegalArgumentException("Missing or invalid issuer field"));
-                this.identifier = asUri(vc.get("id")).orElseThrow(() ->
-                        new IllegalArgumentException("Missing or invalid id field"));
-
-                this.types = asSet(vc.get(TYPE)).orElseGet(Collections::emptySet);
-                this.expiration = asInstant(vc.get("expirationDate")).orElse(Instant.MAX);
-
-                final Map subject = asMap(vc.get("credentialSubject")).orElseThrow(() ->
-                        new IllegalArgumentException("Missing or invalid credentialSubject field"));
-
-                this.creator = asUri(subject.get("id")).orElseThrow(() ->
-                        new IllegalArgumentException("Missing or invalid credentialSubject.id field"));
-
-                // V1 Access Request, using gConsent
-                final Map consent = asMap(subject.get("hasConsent")).orElseThrow(() ->
-                        // Unsupported structure
-                        new IllegalArgumentException("Invalid Access Request: missing consent clause"));
-
-                final Optional<URI> dataSubject = asUri(consent.get("isConsentForDataSubject"));
-                this.recipient = dataSubject.orElse(null);
-                this.modes = asSet(consent.get("mode")).orElseGet(Collections::emptySet);
-                this.resources = asSet(consent.get("forPersonalData")).orElseGet(Collections::emptySet)
-                    .stream().map(URI::create).collect(Collectors.toSet());
-                this.purposes = asSet(consent.get("forPurpose")).orElseGet(Collections::emptySet);
-                this.status = asMap(vc.get("credentialStatus")).flatMap(credentialStatus ->
-                        asSet(credentialStatus.get(TYPE)).filter(statusTypes ->
-                            statusTypes.contains(REVOCATION_LIST_2020_STATUS)).map(x ->
-                                asRevocationList2020(credentialStatus))).orElse(null);
-            } else {
-                throw new IllegalArgumentException("Invalid Access Request: missing VerifiablePresentation type");
-            }
-        }
+    protected AccessRequest(final URI identifier, final String credential, final CredentialData data,
+            final CredentialMetadata metadata) {
+        super(identifier, credential, data, metadata);
     }
-
 
     /**
      * Create an AccessRequest object from a serialized form.
@@ -128,7 +66,7 @@ public class AccessRequest implements AccessCredential {
      */
     public static AccessRequest of(final String serialization) {
         try {
-            return new AccessRequest(serialization);
+            return parse(serialization);
         } catch (final IOException ex) {
             throw new IllegalArgumentException("Unable to read access request", ex);
         }
@@ -148,65 +86,47 @@ public class AccessRequest implements AccessCredential {
         }
     }
 
-    @Override
-    public Set<String> getTypes() {
-        return types;
-    }
-
-    @Override
-    public Set<String> getModes() {
-        return modes;
-    }
-
-    @Override
-    public Optional<Status> getStatus() {
-        return Optional.ofNullable(status);
-    }
-
-    @Override
-    public Instant getExpiration() {
-        return expiration;
-    }
-
-    @Override
-    public URI getIssuer() {
-        return issuer;
-    }
-
-    @Override
-    public URI getIdentifier() {
-        return identifier;
-    }
-
-    @Override
-    public Set<String> getPurposes() {
-        return purposes;
-    }
-
-    @Override
-    public Set<URI> getResources() {
-        return resources;
-    }
-
-    @Override
-    public URI getCreator() {
-        return creator;
-    }
-
-    @Override
-    public Optional<URI> getRecipient() {
-        return Optional.ofNullable(recipient);
-    }
-
-    @Override
-    public String serialize() {
-        return credential;
-    }
-
     static Set<String> getSupportedTypes() {
         final Set<String> types = new HashSet<>();
         types.add("SolidAccessRequest");
         types.add("http://www.w3.org/ns/solid/vc#SolidAccessRequest");
         return Collections.unmodifiableSet(types);
+    }
+
+    static AccessRequest parse(final String serialization) throws IOException {
+        try (final InputStream in = new ByteArrayInputStream(serialization.getBytes())) {
+            // TODO process as JSON-LD
+            final Map<String, Object> data = jsonService.fromJson(in,
+                    new HashMap<String, Object>(){}.getClass().getGenericSuperclass());
+
+            final List<Map> vcs = getCredentialsFromPresentation(data, supportedTypes);
+            if (vcs.size() != 1) {
+                throw new IllegalArgumentException(
+                        "Invalid Access Request: ambiguous number of verifiable credentials");
+            }
+            final Map vc = vcs.get(0);
+
+            if (asSet(data.get(TYPE)).orElseGet(Collections::emptySet).contains("VerifiablePresentation")) {
+                final URI identifier = asUri(vc.get("id")).orElseThrow(() ->
+                        new IllegalArgumentException("Missing or invalid id field"));
+
+                // Extract metadata
+                final CredentialMetadata credentialMetadata = extractMetadata(vc);
+
+                // V1 Access Request, using gConsent
+                final Map consent = extractConsent(vc, "hasConsent");
+
+                final URI recipient = asUri(consent.get("isConsentForDataSubject")).orElse(null);
+                final Set<String> modes = asSet(consent.get("mode")).orElseGet(Collections::emptySet);
+                final Set<URI> resources = asSet(consent.get("forPersonalData")).orElseGet(Collections::emptySet)
+                    .stream().map(URI::create).collect(Collectors.toSet());
+                final Set<String> purposes = asSet(consent.get("forPurpose")).orElseGet(Collections::emptySet);
+                final CredentialData credentialData = new CredentialData(resources, modes, purposes, recipient);
+
+                return new AccessRequest(identifier, serialization, credentialData, credentialMetadata);
+            } else {
+                throw new IllegalArgumentException("Invalid Access Request: missing VerifiablePresentation type");
+            }
+        }
     }
 }
