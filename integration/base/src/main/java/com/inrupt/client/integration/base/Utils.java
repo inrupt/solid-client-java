867d62c96e9c649aaf0da35f5dcdf35f0685f05b
Timea Turdean <4144203+timea-solid@users.noreply.github.com>
2023-07-26 13:47:39 +0200

    try with a different custom client

--- integration/base/src/main/java/com/inrupt/client/integration/base/Utils.java
+++ integration/base/src/main/java/com/inrupt/client/integration/base/Utils.java
@@ -24,12 +24,18 @@ import static com.inrupt.client.vocabulary.RDF.type;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.inrupt.client.Client;
 import com.inrupt.client.Headers;
 import com.inrupt.client.Request;
 import com.inrupt.client.Response;
+import com.inrupt.client.auth.Challenge;
+import com.inrupt.client.auth.Credential;
+import com.inrupt.client.auth.ReactiveAuthorization;
+import com.inrupt.client.auth.Session;
 import com.inrupt.client.core.DefaultClient;
 import com.inrupt.client.okhttp.OkHttpService;
 import com.inrupt.client.solid.*;
+import com.inrupt.client.spi.HttpService;
 import com.inrupt.client.spi.RDFFactory;
 import com.inrupt.client.spi.ServiceProvider;
 import com.inrupt.client.util.URIBuilder;
@@ -48,6 +54,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.io.IOUtils;
@@ -304,9 +312,15 @@ public final class Utils {
             newBuilder.hostnameVerifier((hostname, session) -> true);
 
             LOGGER.info("Working with a custom OkHttp client which trusts all certificates.");
-            return SolidSyncClient.getClientBuilder()
+            /*return SolidSyncClient.getClientBuilder()
                     .client(DefaultClient.newBuilder()
                             .withInstance(OkHttpService.ofOkHttpClient(newBuilder.build())).build())
+                    .build();*/
+            return SolidSyncClient.getClientBuilder().client(
+                    CustomOkHttpCLient
+                            .newBuilder()
+                            .withInstance(OkHttpService.ofOkHttpClient(newBuilder.build()))
+                            .build())
                     .build();
         }
 
@@ -315,6 +329,116 @@ public final class Utils {
 
     private Utils() {
         // Prevent instantiation
+    }
+
+    public static class CustomOkHttpCLient implements Client {
+        private static final int UNAUTHORIZED = 401;
+        private static final String AUTHORIZATION = "Authorization";
+        private static final String DPOP = "DPoP";
+        private final Logger LOGGER = LoggerFactory.getLogger(DefaultClient.class);
+
+        private final ReactiveAuthorization authHandler = new ReactiveAuthorization();
+        private final HttpService httpClient;
+        private final Session clientSession;
+
+        CustomOkHttpCLient(final HttpService httpClient) {
+            this(httpClient, Session.anonymous());
+        }
+
+        CustomOkHttpCLient(final HttpService httpClient, final Session session) {
+            Objects.requireNonNull(httpClient, "Http client may not be null!");
+            Objects.requireNonNull(session, "Session may not be null!");
+            this.httpClient = httpClient;
+            this.clientSession = session;
+        }
+
+        @Override
+        public Client session(final Session session) {
+            return new CustomOkHttpCLient(this.httpClient, session);
+        }
+
+        @Override
+        public <T> CompletionStage<Response<T>> send(final Request request,
+                                                     final Response.BodyHandler<T> responseBodyHandler) {
+            // if there is already an auth header, just pass the request directly through
+            if (request.headers().firstValue(AUTHORIZATION).isPresent()) {
+                LOGGER.debug("Sending user-supplied authorization, skipping Solid authorization handling");
+                return httpClient.send(request, responseBodyHandler);
+            }
+
+            // Check session cache for a relevant access token
+            return clientSession.fromCache(request)
+                // Use that token, if present
+                .map(token -> httpClient.send(upgradeRequest(request, token), responseBodyHandler))
+                // Otherwise perform the regular HTTP authorization dance
+                .orElseGet(() -> httpClient.send(request, responseBodyHandler)
+                    .thenCompose(res -> {
+                        if (res.statusCode() == UNAUTHORIZED) {
+                            final List<Challenge> challenges = Headers.WwwAuthenticate
+                                .parse(res.headers().allValues("WWW-Authenticate").toArray(new String[0]))
+                                .getChallenges();
+
+                            return authHandler.negotiate(clientSession, request, challenges)
+                                .thenCompose(token -> token.map(t ->
+                                            httpClient.send(upgradeRequest(request, t), responseBodyHandler))
+                                    .orElseGet(() -> CompletableFuture.completedFuture(res)))
+                                .exceptionally(err -> {
+                                    LOGGER.debug("Unable to negotiate an authentication token: {}", err.getMessage());
+                                    return res;
+                                });
+                        }
+                        return CompletableFuture.completedFuture(res);
+                    }));
+        }
+
+        Request upgradeRequest(final Request request, final Credential token) {
+            final Request.Builder builder = Request.newBuilder()
+                    .uri(request.uri())
+                    .method(request.method(), request.bodyPublisher().orElseGet(Request.BodyPublishers::noBody));
+
+            LOGGER.debug("Sending upgraded request: {}", request.uri());
+            request.timeout().ifPresent(builder::timeout);
+            request.headers().asMap().forEach((name, values) -> {
+                for (final String value : values) {
+                    builder.header(name, value);
+                }
+            });
+
+            // Use setHeader to overwrite any possible existing authorization header
+            builder.setHeader(AUTHORIZATION, String.join(" ", token.getScheme(), token.getToken()));
+            if (DPOP.equalsIgnoreCase(token.getScheme())) {
+                token.getProofThumbprint().flatMap(jkt -> clientSession.generateProof(jkt, request))
+                        .ifPresent(proof -> builder.setHeader(DPOP, proof));
+            }
+
+            return builder.build();
+        }
+
+        public static Client.Builder newBuilder() {
+            return new CustomOkHttpCLient.Builder();
+        }
+
+        /**
+         * The builder of the default client implementation.
+         */
+        public static class Builder implements Client.Builder {
+
+            private HttpService instance;
+
+            @Override
+            public Client.Builder withInstance(final HttpService instance) {
+                this.instance = instance;
+                return this;
+            }
+
+            @Override
+            public Client build() {
+                if (instance == null) {
+                    return new CustomOkHttpCLient(ServiceProvider.getHttpService());
+                }
+                return new CustomOkHttpCLient(instance);
+            }
+        }
     }
 
 }
