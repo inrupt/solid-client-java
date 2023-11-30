3debe85b642a9b3cbf25f00fddfe1f907fc820fb
acoburn <acoburn@users.noreply.github.com>
2023-11-30 10:46:47 +0000

    deploy: 5e482622ca154eed15fa2301727e9ebce880a3de

---
+++ 1.2.0-SNAPSHOT/examples/cli/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.inrupt.client.examples.cli","l":"AppConfig"},{"p":"com.inrupt.client.examples.cli","l":"SolidApp"}];updateSearchResults();
