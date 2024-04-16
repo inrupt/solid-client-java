ca829b2baa5967c015cdd409e88f067cd2d93dbf
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-04-16 14:54:36 +0200

    Revert "Replace ProblemDetailsData w prv. setters"
    This reverts commit 499164d6d61338e0ffcc4b724f5793e295f6c2cf.

---
+++ api/src/main/java/com/inrupt/client/ProblemDetailsData.java
@@ -0,0 +1,55 @@
+package com.inrupt.client;
+
+import java.net.URI;
+
+/**
+ * This package-private mutable class is used for JSON deserialization.
+ * Once instantiated, it is used to build an immutable {@link ProblemDetails}.
+ */
+class ProblemDetailsData {
+    private URI type;
+    private String title;
+    private String details;
+    private int status;
+    private URI instance;
+
+    public URI getType() {
+        return this.type;
+    };
+
+    public String getTitle() {
+        return this.title;
+    };
+
+    public String getDetails() {
+        return this.details;
+    };
+
+    public int getStatus() {
+        return this.status;
+    };
+
+    public URI getInstance() {
+        return this.instance;
+    };
+
+    public void setType(final URI type) {
+        this.type = type;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public void setDetails(final String details) {
+        this.details = details;
+    }
+
+    public void setStatus(final int status) {
+        this.status = status;
+    }
+
+    public void setInstance(final URI instance) {
+        this.instance = instance;
+    }
+}
